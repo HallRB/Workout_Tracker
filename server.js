2197881810f051b8ebdbaabacf8aea89e2cfc395
HallRB <Richardbrianhall@gmail.com>
2020-11-18 17:29:36 -0600

    db test

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-app.use(morgan("tiny"))
+app.use(morgan("dev"))
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
